16d4dec464c1f7d538834e06b1f70d445b8ea9d9
fredgad <31152572+fredgad@users.noreply.github.com>
2019-03-08 01:51:16 +0300

    sad

--- src/js/dryfog.js
+++ src/js/dryfog.js
@@ -1,39 +1,61 @@
-$(function() {
+onload = ()=> {
+  //Variables
+  let wrap = document.getElementById("wrap"),
+  rngWidth = document.getElementById("rngWidth"),
+  rngHeight = document.getElementById("rngHeight"),
+  cont = document.getElementById("cont"),
+  bttn = document.getElementById("bttn"),
+  turn = document.getElementById("turn"),
+  tran = document.getElementById("tran"),   
+  num = document.getElementById("num"),
+  width = wrap.offsetWidth,
+  height = wrap.offsetHeight;
 
-$("#home").click(()=> {
-  forSecond("#first");
+//Events
+bttn.addEventListener("click", ()=> {
+  cut(num.value);
+  console.log(num.value);
 });
-$("#top-angle, #first-arrow, #special").click(()=> {
-  forSecond("#second");
+turn.addEventListener("click", ()=> {
+  anim("turn", "2");
 });
-$("#second-angle, #second-arrow, #prod").click(()=> {
-  forSecond("#third");
+tran.addEventListener("click", ()=> {
+  anim("tran", "1");
 });
-$("#third-angle, #third-arrow, #share").click(()=> {
-  forSecond("#fourth");
+rngWidth.addEventListener("input", ()=> {
+  cut(rngWidth.value);
+  console.log(rngWidth.value);
 });
 
-function forSecond(addres) {
-  this.add = addres;
-  let x = $('body').scrollTop(), z = $(this.add).offset().top
+//Functions
+function cut(size) {
+  //Clean container
+  cont.innerHTML = '';
 
-  if (x < $(this.add).offset().top - 26) {
-    $('body').scrollTop(x+=25);
-    requestAnimationFrame(()=> {
-      forSecond(this.add);
-    });
-  } else if (x > $(this.add).offset().top + 26) {
-    $('body').scrollTop(x-=25);
-    requestAnimationFrame(()=> {
-      forSecond(this.add);
-    });
+  //Fill container
+  for(let x = 0; x < size; x++) {
+      cont.innerHTML += '<div class="part"></div>';
   }
-};
-  
- 
+
+  let parts = document.getElementsByClassName("part"),   
+  pud = 100 / num.value,
+  pos = 0;
+
+  for(let y = 0; y < parts.length; y++) {
+      parts[y].style.width = width / num.value + "px";
+      parts[y].style.height = height + "px";
+
+  pos += pud; 
+  parts[y].style.backgroundSize = num.value * 100 + "%"; parts[y].style.backgroundPosition = pos + "% 0";
+  }
+}
+
+function anim(animType, animSpeed) {
+  let parts = document.getElementsByClassName("part");
+  for(let x = 0;x < parts.length; x++) {
+      parts[x].style.animation = `${animType} ${animSpeed}s linear ${x/50}s`;
+  }
+} 
 
 
-
-  
-
-});
+}
